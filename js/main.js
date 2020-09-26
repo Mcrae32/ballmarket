94dfca984433f4d72859f2a0a6637e3738ae4e91
Денис Ковалев <deniskovalev@MacBook-Air-Denis.local>
2020-09-26 03:30:35 +0300

    hide and show mobile filter and menu

--- js/main.js
+++ js/main.js
@@ -175,4 +175,23 @@ $(".sub-menu").position({
 });
 */
 
+//show mobile menu and filter
+$(document).ready(function() {
+    $('.menu-catalog').click( function(){
+        $('.home-page-menu-wrap').addClass('mobile_show');
+    } )
+
+    $('[data-target="hidden-filters"]').click( function(){
+        $('.home-page-menu-wrap').removeClass('mobile_show');
+    } )
+
+    $('.flter-catalog-mobile').click( function(){
+        $('.filter-wrap').addClass('mobile_show');
+    } )
+
+    $('[data-target="hidden-filters"]').click( function(){
+        $('.filter-wrap').removeClass('mobile_show');
+    } )
+});
+
 
